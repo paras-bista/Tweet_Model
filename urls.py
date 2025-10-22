999e399dda1763ed62ef3912c19516d15e564e6f
parasbita18 <parasbista18@gmail.com>
2025-10-22 08:44:10 +0545

    Fix homepage layout and improve unauthenticated user view

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.contrib.auth.views import LogoutView
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
-    path('', include('tweet.urls')),  # your tweet app URLs
-]
